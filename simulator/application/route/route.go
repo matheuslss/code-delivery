600ecd45544cb117b7c5261d42868f5ca9d04f9e
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-15 20:23:50 -0300

    Criar metodo loadPositions

--- simulator/application/route/route.go
+++ simulator/application/route/route.go
@@ -1,5 +1,15 @@
 package route
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
 type Route struct {
 	ID        string
 	ClientID  string
@@ -9,4 +19,40 @@ type Route struct {
 type Position struct {
 	Lat  float64
 	Long float64
+}
+
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("Não foi possível obter o identificador da rota.")
+	}
+
+	file, err := os.Open("destinations/" + r.ID + ".txt")
+	if err != nil {
+		logrus.Error(err)
+		return errors.New("Não foi possível obter as rotas.")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			logrus.Error(err)
+			return errors.New("Não foi possível obter as rotas.")
+		}
+		long, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			logrus.Error(err)
+			return errors.New("Não foi possível obter as rotas.")
+		}
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
+			Long: long,
+		})
+	}
+
+	return nil
 }
--- simulator/go.mod
+++ simulator/go.mod
@@ -1,3 +1,9 @@
 module github.com/matheuslss/code-delivery
 
 go 1.16
+
+require (
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/sirupsen/logrus v1.8.1
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+)

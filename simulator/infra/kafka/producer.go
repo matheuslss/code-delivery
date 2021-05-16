82f6f8bc751982b829f12161603549f792fa71d9
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-15 21:16:45 -0300

    Criar producer

---
+++ simulator/infra/kafka/producer.go
@@ -0,0 +1,37 @@
+package kafka
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/sirupsen/logrus"
+)
+
+func NewKafkaProducer() *ckafka.Producer {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+	}
+	producer, err := ckafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return producer
+}
+
+func Publish(msg, topic string, producer *ckafka.Producer) error {
+	message := &ckafka.Message{
+		TopicPartition: ckafka.TopicPartition{Topic: &topic, Partition: ckafka.PartitionAny},
+		Value:          []byte(msg),
+	}
+
+	err := producer.Produce(message, nil)
+	if err != nil {
+		logrus.Error(err)
+		return errors.New("Não foi possível publicar a mensagem.")
+	}
+
+	return nil
+}
